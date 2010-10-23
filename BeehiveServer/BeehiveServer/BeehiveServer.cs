2f371d340310c4c652d5642433b4caee749c1cb9
Bhavya Kashyap <bhkashyap@gmail.com>
2010-10-23 19:49:50 -0400

    first real commit

---
+++ BeehiveServer/BeehiveServer/BeehiveServer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Application
+{
+
+    class Program
+    {
+        //global variables
+        static Int32 portNo;
+        static System.Net.IPAddress localAdd;
+        static TcpListener listener;
+
+        static void header()
+        {
+            Console.WriteLine("===============================================================================");
+            Console.WriteLine("                          Beehive Server v 1.0.0.0                             ");
+            Console.WriteLine("                             by Bhavya Kashyap                                 ");
+            Console.WriteLine("=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
+        }
+
+        static void intro()
+        {
+            Console.WriteLine("Welcome to Beehive Server. Please select an option below:\n");
+            settings();
+        }
+
+        static void settings()
+        {
+            Console.WriteLine("d - connect with default settings on port 500.");
+            Console.WriteLine("n - connect with new settings");
+
+            string option = Console.ReadLine();
+            if (option.Equals("n"))
+            {
+                Console.WriteLine("Port Number:");
+                string internalPort = Console.ReadLine();
+
+                try
+                {
+                    portNo = Int32.Parse(internalPort);
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid port number provided.");
+                    return;
+                }
+            }
+            else if (option.Equals("d"))
+            {
+                try
+                {
+                    portNo = Int32.Parse("500");
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid port number provided.");
+                    return;
+                }
+            }
+
+        }
+
+        static Int32 startListener()
+        {
+            string internalIP = "127.0.0.1";
+            try
+            {
+                localAdd = System.Net.IPAddress.Parse(internalIP);
+            }
+            catch
+            {
+                Console.WriteLine("No localhost IP set.");
+                return 0;
+            }
+
+            listener = new TcpListener(localAdd, portNo);
+            try
+            {
+                listener.Start();
+                return 1;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("TCP Listener failed to start." + e);
+                return 0;
+            }
+        }
+
+        static void acceptTCPRequest()
+        {
+            while (true)
+            {
+                ChatClient user = new ChatClient(listener.AcceptTcpClient());
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            header();
+            intro();
+            if (startListener() == 1)
+            {
+                Console.WriteLine("Setting up listener at " + localAdd + " on port " + portNo);
+                Console.WriteLine("Waiting for connections...");
+                acceptTCPRequest();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BeehiveServer/BeehiveServer/ChatClient.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Application
+{
+    public class ChatClient
+    {
+        public HashSet<string> AllClients = new HashSet<string>();
+        private TcpClient _client;
+        private string _clientIP;
+        private string _clientNick;
+        private byte[] data;
+        private bool ReceiveNick = true;
+
+        public ChatClient(TcpClient client)
+        {
+            _client = client;
+            _clientIP = client.Client.RemoteEndPoint.ToString();
+            AllClients.Add(_clientIP);
+            data = new byte[_client.ReceiveBufferSize];
+            Console.WriteLine("New Connection at: " + _clientIP);
+            _client.GetStream().BeginRead(data, 0, data.Length, new AsyncCallback(ReceiveMessage), null);
+
+
+        }
+
+        public void ReceiveMessage(IAsyncResult ar)
+        {
+            int bytesRead;
+            try
+            {
+                lock (_client.GetStream())
+                {
+                    bytesRead = _client.GetStream().EndRead(ar);
+                }
+
+                //client has disconnected
+                if (bytesRead < 1)
+                {
+                    AllClients.Remove(_clientIP);
+                    Broadcast(_clientNick + " has left the chat.");
+                    return;
+                }
+                else
+                {
+                    string messageReceived = System.Text.Encoding.ASCII.GetString(data, 0, bytesRead);
+
+                    if (ReceiveNick)
+                    {
+                        _clientNick = messageReceived;
+                        Broadcast(_clientNick + " has joined the chat.");
+                        ReceiveNick = false;
+                    }
+                    else
+                    {
+                        Broadcast(_clientNick + " says:\n" + messageReceived);
+                    }
+                }
+
+                lock (_client.GetStream())
+                {
+                    _client.GetStream().BeginRead(data, 0, _client.ReceiveBufferSize, ReceiveMessage, null);
+                }
+
+            }
+            catch
+            {
+                AllClients.Remove(_clientIP);
+                Broadcast(_clientNick + " has left the chat.");
+            }
+
+        }
+
+        public void SendMessage(string message)
+        {
+            try
+            {
+                System.Net.Sockets.NetworkStream ns;
+                lock (_client.GetStream())
+                {
+                    ns = _client.GetStream();
+                }
+
+                byte[] bytesToSend = System.Text.Encoding.ASCII.GetBytes(message);
+                ns.Write(bytesToSend, 0, bytesToSend.Length);
+                ns.Flush();
+            }
+            catch
+            {
+                Console.WriteLine("Error: could not send");
+            }
+
+        }
+
+        public void Broadcast(string message)
+        {
+            Console.WriteLine(message);
+            foreach (string c in AllClients)
+            {
+                SendMessage(message);
+            }
+
+        }
+
+
+    }
+}
